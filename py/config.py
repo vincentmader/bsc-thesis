a973137ac98a72046812d21c1a7a1e6c833c8290
Vincent Mader <vincent.mader@icloud.com>
2019-12-11 13:07:14 +0100

    moved python methods into temporary folder

---
+++ py/config.py
@@ -0,0 +1,12 @@
+
+import os
+
+
+# declare directory path strings for iCloud docs and Bachelor repository
+CLOUD_DIR = '/Users/meister/Library/Mobile Documents/com~apple~CloudDocs'
+BACHELOR_DIR = os.path.join(CLOUD_DIR, 'Uni/Bachelor-Arbeit/')
+# declare directory path strings for fargo2d1d files and figure save location
+FARGO_DIR = os.path.join(BACHELOR_DIR, 'fargo2d1d')
+FIGURE_DIR = os.path.join(BACHELOR_DIR, 'figures')
+
+
